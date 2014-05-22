ba8ab40129b478b726af3f07ed0973e0bf864354
leomastakusuma <leomastakusuma@gmail.com>
2014-05-22 20:24:45 +0700

    asdasd

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -14,7 +14,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     protected function _initAutoload()
     {
         $modelLoader    =  new Zend_Application_Module_Autoloader(array(
-            'namespace' => '', 'basePath' => APPLICATION_PATH.'/modules'
+            'namespace' => '', 'basePath' => APPLICATION_PATH
             
         ));
         $namespace = new Zend_Session_Namespace('Zend_Auth');
@@ -34,7 +34,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $front = Zend_Controller_Front::getInstance();
         // echo "<pre>";
         // print_r($auth);die;
-        // $front->registerPlugin(new Plugin_AccessCheck($acl, $auth));
+        $front->registerPlugin(new Plugin_AccessCheck($acl, $auth));
 //        $front->registerPlugin(new Plugin_AccessCheck());
     }
     
